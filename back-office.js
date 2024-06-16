9bfc0c3d2d212fa0d3c66761cdc4d194a74a1d9e
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-06-16 18:02:44 +0200

    aggiunto pulstante modifica e fix errori immagini card

--- back-office.js
+++ back-office.js
@@ -1,55 +1,122 @@
-const apiKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZmE4YjdjMjM5YzAwMTUyZjRiNDQiLCJpYXQiOjE3MTgzNTM1NTUsImV4cCI6MTcxOTU2MzE1NX0.nMG8gyEtURlUmqf7sf1mN65Bp38dmp4xbI17KPk0YJc";
+const urlParams = new URLSearchParams(window.location.search);
+const animalId = urlParams.get("id");
 
-document.getElementById("form").addEventListener("submit", function (event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  const apiKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZmE4YjdjMjM5YzAwMTUyZjRiNDQiLCJpYXQiOjE3MTgzNTM1NTUsImV4cCI6MTcxOTU2MzE1NX0.nMG8gyEtURlUmqf7sf1mN65Bp38dmp4xbI17KPk0YJc";
 
-  const name = document.getElementById("name").value;
-  const description = document.getElementById("description").value;
-  const brand = document.getElementById("brand").value;
-  const imageUrl = document.getElementById("url").value;
-  const price = document.getElementById("price").value;
+  const form = document.getElementById("form");
+  const nameInput = document.getElementById("name");
+  const descriptionInput = document.getElementById("description");
+  const brandInput = document.getElementById("brand");
+  const priceInput = document.getElementById("price");
+  const urlInput = document.getElementById("url");
 
-  const newAnimal = {
-    name: name,
-    description: description,
-    brand: brand,
-    imageUrl: imageUrl,
-    price: price,
-  };
+  if (animalId) {
+    // aggiorna il form alla modifica
+    document.getElementById("form").setAttribute("data-animal-id", animalId);
+    fetchAnimal(animalId);
+  } else {
+  }
 
-  fetch("https://striveschool-api.herokuapp.com/api/product/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-    body: JSON.stringify(newAnimal),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response error");
-      }
-      return response.json();
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const description = descriptionInput.value;
+    const brand = brandInput.value;
+    const imageUrl = urlInput.value;
+    const price = priceInput.value;
+
+    const newAnimal = {
+      name: name,
+      description: description,
+      brand: brand,
+      imageUrl: imageUrl,
+      price: price,
+    };
+
+    if (animalId) {
+      //  PUT requestse si modifica un animale
+      updateAnimal(animalId, newAnimal);
+    } else {
+      // POST request se si aggiunge
+      createAnimal(newAnimal);
+    }
+  });
+
+  function fetchAnimal(animalId) {
+    fetch(`https://striveschool-api.herokuapp.com/api/product/${animalId}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
     })
-    .then((data) => {
-      console.log("Success:", data);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Errore di rete: " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((animal) => {
+        // Popola il form con i dati dell'animale da modificare
+        nameInput.value = animal.name;
+        descriptionInput.value = animal.description;
+        brandInput.value = animal.brand;
+        priceInput.value = animal.price;
+        urlInput.value = animal.imageUrl;
+      })
+      .catch((error) => {
+        console.error("Errore durante il fetch dell'animale:", error);
+      });
+  }
 
-      document.getElementById("form").reset();
+  function createAnimal(newAnimal) {
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(newAnimal),
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        alert("Animale aggiunto con successo!");
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  function updateAnimal(animalId, updatedAnimal) {
+    fetch(`https://striveschool-api.herokuapp.com/api/product/${animalId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(updatedAnimal),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("errore");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        alert("Modifica avvenuta con successo!");
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 });
-/* const reset = document.getElementById("deleteButton"); */
-
-/* reset.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const _id = (document.getElementById("_id").value = "");
-  const name = (document.getElementById("name").value = "");
-  const description = (document.getElementById("description").value = "");
-  const price = (document.getElementById("price").value = "");
-  const imageUrl = (document.getElementById("imageUrl").value = "");
-  const brand = (document.getElementById("brand").value = "");
-}); */
--- homepage.js
+++ homepage.js
@@ -1,85 +1,103 @@
-const apiKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZmE4YjdjMjM5YzAwMTUyZjRiNDQiLCJpYXQiOjE3MTgzNTM1NTUsImV4cCI6MTcxOTU2MzE1NX0.nMG8gyEtURlUmqf7sf1mN65Bp38dmp4xbI17KPk0YJc";
+// homepage.js
 
-const fetchanimals = () => {
-  fetch("https://striveschool-api.herokuapp.com/api/product/", {
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("errore " + response.statusText);
-      }
-      return response.json();
+document.addEventListener("DOMContentLoaded", function () {
+  const apiKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZmE4YjdjMjM5YzAwMTUyZjRiNDQiLCJpYXQiOjE3MTgzNTM1NTUsImV4cCI6MTcxOTU2MzE1NX0.nMG8gyEtURlUmqf7sf1mN65Bp38dmp4xbI17KPk0YJc";
+
+  const fetchanimals = () => {
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
     })
-    .then((animals) => {
-      console.log("animals", animals);
-      const row = document.getElementById("cardsContainer");
-      row.innerHTML = "";
-      animals.forEach((element) => {
-        const col = document.createElement("div");
-        col.classList.add("col");
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Errore di rete: " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((animals) => {
+        console.log("animals", animals);
+        const row = document.getElementById("cardsContainer");
+        row.innerHTML = "";
+        animals.forEach((element) => {
+          const col = document.createElement("div");
+          col.classList.add("col");
 
-        const card = document.createElement("div");
-        card.classList.add("card", "bg-dark", "text-light");
+          const card = document.createElement("div");
+          card.classList.add("card", "bg-dark", "text-light");
 
-        const img = document.createElement("img");
-        img.src = element.imageUrl;
-        img.classList.add("card-img-top");
+          const img = document.createElement("img");
+          img.src = element.imageUrl;
+          img.classList.add("card-img-top");
 
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
+          const cardBody = document.createElement("div");
+          cardBody.classList.add("card-body");
 
-        const description = document.createElement("p");
-        description.textContent = element.description;
+          const description = document.createElement("p");
+          description.textContent = element.description;
 
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
-        cardTitle.textContent = element.name;
+          const cardTitle = document.createElement("h5");
+          cardTitle.classList.add("card-title");
+          cardTitle.textContent = element.name;
 
-        const cardPrice = document.createElement("p");
-        cardPrice.classList.add("card-text");
-        cardPrice.textContent = "prezzo: " + element.price + "€";
+          const cardPrice = document.createElement("p");
+          cardPrice.classList.add("card-text");
+          cardPrice.textContent = "Prezzo: " + element.price + "€";
 
-        const button = document.createElement("button");
-        button.classList.add("btn", "btn-outline-danger");
-        button.innerText = "delete";
-        button.addEventListener("click", (event) => {
-          event.target.closest(".col").remove();
+          const deleteButton = document.createElement("button");
+          deleteButton.classList.add("btn", "btn-outline-danger", "me-2");
+          deleteButton.innerText = "Delete";
+          deleteButton.addEventListener("click", () => {
+            deleteAnimal(element._id);
+          });
+
+          const editButton = document.createElement("a");
+          editButton.classList.add("btn", "btn-outline-primary");
+          editButton.innerText = "Modifica";
+          editButton.href = `./back-office.html?id=${element._id}`;
+
+          const buttonContainer = document.createElement("div");
+          buttonContainer.classList.add("d-flex", "justify-content-between");
+          buttonContainer.appendChild(deleteButton);
+          buttonContainer.appendChild(editButton);
+
+          cardBody.appendChild(cardTitle);
+          cardBody.appendChild(description);
+          cardBody.appendChild(cardPrice);
+          cardBody.appendChild(buttonContainer);
+          card.appendChild(img);
+          card.appendChild(cardBody);
+          col.appendChild(card);
+          row.appendChild(col);
         });
-
-        const priceAndButtonContainer = document.createElement("div");
-        priceAndButtonContainer.classList.add(
-          "d-flex",
-          "justify-content-between",
-          "priceAndButtonContainer"
-        );
-
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(description);
-        cardBody.appendChild(cardPrice);
-        priceAndButtonContainer.appendChild(button);
-        cardBody.appendChild(priceAndButtonContainer);
-        card.appendChild(img);
-        card.appendChild(cardBody);
-        col.appendChild(card);
-        row.appendChild(col);
+      })
+      .catch((error) => {
+        console.error("Errore durante il fetch degli animali:", error);
       });
+  };
 
-      const main = document.querySelector("main");
-      const footer = document.createElement("footer");
-      footer.classList.add("mt-4", "text-center", "text-secondary");
-
-      const pFooter = document.createElement("p");
-      pFooter.innerHTML = "copyright kun (131213121312)";
-      footer.appendChild(pFooter);
-
-      main.insertAdjacentElement("afterend", footer);
+  function deleteAnimal(animalId) {
+    fetch(`https://striveschool-api.herokuapp.com/api/product/${animalId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
     })
-    .catch((err) => console.error("Fetch error:", err));
-};
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Errore di rete: " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Animale eliminato:", data);
+        fetchanimals(); // Aggiorna la lista degli animali dopo l'eliminazione
+      })
+      .catch((error) => {
+        console.error("Errore durante l'eliminazione dell'animale:", error);
+      });
+  }
 
-window.addEventListener("DOMContentLoaded", () => {
-  fetchanimals();
+  fetchanimals(); //  funzione fetchanimals avvio della pagina
 });
